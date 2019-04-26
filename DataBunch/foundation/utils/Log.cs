b4d3f4389fdb628c79c42361952e98b0613f4677
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2019-04-26 22:20:49 +0200

    Adds success method to Log class

--- DataBunch/foundation/utils/Log.cs
+++ DataBunch/foundation/utils/Log.cs
@@ -10,7 +10,18 @@ namespace DataBunch.foundation.utils
             Console.ForegroundColor = ConsoleColor.Blue;
 
             Console.Write("[INFO]");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(" " + message);
 
+            Console.ResetColor();
+        }
+
+        public static void success(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.Write("[SUCCESS]");
             Console.BackgroundColor = ConsoleColor.Black;
             Console.WriteLine(" " + message);
 
