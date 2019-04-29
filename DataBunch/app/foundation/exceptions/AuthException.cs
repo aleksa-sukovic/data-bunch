4695b2a919f5017d469a7885f5794905f1ee75a1
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2019-04-30 01:59:04 +0200

    Adds Storage exception
    Updates AuthException constructor

--- DataBunch/app/foundation/exceptions/AuthException.cs
+++ DataBunch/app/foundation/exceptions/AuthException.cs
@@ -2,6 +2,6 @@ namespace DataBunch.app.foundation.exceptions
 {
     public class AuthException: BaseException
     {
-        public AuthException(string message = "") : base("Auth Error", "Please check your credentials.", message) { }
+        public AuthException(string message = "") : base("Auth Error", message, message) { }
     }
 }
